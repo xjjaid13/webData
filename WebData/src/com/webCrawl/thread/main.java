2d44b46f0dafe1ac4218df452f777f25117e4040
Administrator <Administrator@PC-201312051201.lan>
2014-05-04 08:20:49 +0800

    Signed-off-by: Administrator <Administrator@PC-201312051201.lan>

---
+++ WebData/src/com/webCrawl/thread/main.java
@@ -0,0 +1,73 @@
+package com.webCrawl.thread;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.util.DataHandle;
+import com.webCrawl.MainTest1;
+import com.webCrawl.entity.CrawlBug;
+import com.webCrawl.extract.IExtract;
+import com.webCrawl.store.IStoreDB;
+import com.webCrawl.store.IStorePage;
+
+public class main {
+
+	static CrawlBug crawlBug;
+	
+	static IStorePage IStorePage;
+	
+	static IExtract commonExtract;
+	
+	static IStoreDB storeDB;
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[] {"conf.xml"});
+		
+		crawlBug = new CrawlBug();
+		crawlBug.setBugId(2);
+		crawlBug.setSavePath("D:/crawl2/"+crawlBug.getBugId()+"/");
+		crawlBug.setSeedUrl("http://192.168.199.164:9080/cms/");
+		crawlBug.setDomain("192.168.199.164");
+		crawlBug.setBugName("bug Num.2");
+		
+		storeDB = context.getBean("storeDB",IStoreDB.class);
+		storeDB.init(crawlBug);
+		
+		commonExtract = context.getBean("commonExtract",IExtract.class);
+		
+		IStorePage = context.getBean("storePageHtml",IStorePage.class);
+		IStorePage.init(crawlBug);
+
+		storeDB.save(crawlBug.getSeedUrl());
+		
+		ExecutorService cachedThreadPool = Executors.newCachedThreadPool();  
+		
+		for (int i = 0; i < 1; i++) {
+			cachedThreadPool.execute(new Runnable() {
+				public void run() {
+					while(true){
+						String newUrl = storeDB.returnLink();
+						
+//						if(DataHandle.isNullOrEmpty(newUrl)){
+//							break;
+//						}
+						
+						String savePath = IStorePage.store(newUrl);
+						
+						List<Object> urlList = commonExtract.extractHtml(savePath);
+						
+						storeDB.save(urlList);
+					}
+					
+				}
+			});
+		}
+		
+	}
+	
+}
